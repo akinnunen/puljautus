870beed4ea36f2593529d37b9ff89f763e6db3ce
Markus Sammallahti <markus.sammallahti@avaus.fi>
2013-11-01 19:53:29 +0200

    Populate game mode buttons from model

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,33 @@
 
 angular.module('vagrantApp')
   .controller('MainCtrl', function ($scope, state, nameGenerator) {
-    $scope.awesomeThings = [
-      'Kalja',
-      'Karkki',
-      'Kalja'
+    $scope.gameModes = [
+    	{
+    		id: "names",
+    		name: "Names",
+    		enabled: true
+    	},
+    	{
+    		id: "projects",
+    		name: "Projects",
+    		enabled: false
+    	},
+    	{
+    		id: "competence",
+    		name: "Competence",
+    		enabled: false
+    	},
+    	{
+    		id: "other",
+    		name: "Something else",
+    		enabled: false
+    	}
     ];
+
+    $scope.play = function(mode) {
+    	console.log("Play: " + mode.id);
+    }
+
     state.gameRounds = 4;
     state.currentGameRound = 1;
 
