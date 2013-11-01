59e13a043c147a915b8d7631e4d6e2bd21dd5957
Markus Sammallahti <markus.sammallahti@avaus.fi>
2013-11-01 20:06:19 +0200

    Fix used variable name in test description

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('should attach a list of gameModes to the scope', function () {
     expect(scope.gameModes.length).toBe(4);
   });
 });
