ed8ceca4225335550ceac427fff29bd58d06535a
Kalle Bertell <carl-petter.bertell@avaus.fi>
2013-11-01 20:01:21 +0200

    round timer done

--- app/scripts/services/round_timer.js
+++ app/scripts/services/round_timer.js
@@ -1,8 +1,47 @@
 angular.module('vagrantApp')
-    .factory('roundTimer', function () {
-        'use strict';
+  .factory('roundTimer', function () {
+    'use strict';
 
-        return {
-        	timeLeftInMillis: 30000
+    var RoundTimer = function() {
+      this.timeLeftInMillis = 30000;
+      this.running = false;
+    }
+
+    RoundTimer.prototype.start = function() {
+      this.running = true;
+      this.activeTimeout = null;
+
+      var timer = this;
+
+      var countdown = function() {
+        timer.decrementSecond();
+        if (timer.leftInMillis > 0) {
+          timer.activeTimeout = setTimeout(countdown, 1000)
+        } else {
+          timer.running = false;
+          timer.timeLeftInMillis = 0;
         }
-    });
+      }
+
+      setTimeout(countdown, 1000);
+    }
+
+    RoundTimer.prototype.decrementSecond = function() {
+      this.timeLeftInMillis = this.timeLeftInMillis - 1000;
+    }
+
+    RoundTimer.prototype.incrementSecond = function() {
+      this.timeLeftInMillis = this.timeLeftInMillis + 1000;
+    }
+
+    RoundTimer.prototype.reset = function() {
+      if (this.activeTimeout) {
+        clearTimeout(this.activeTimeout);
+      }
+
+      this.running = false;
+      this.timeLeftInMillis = 30000;
+    }
+
+    return new RoundTimer();
+  });
--- test/spec/services/round_timer_spec.js
+++ test/spec/services/round_timer_spec.js
@@ -13,7 +13,32 @@ describe('roundTimer', function () {
   }));
 
   it('should be inited to 30 secs', function () {
-
     expect(target.timeLeftInMillis).toBe(30000);
+    expect(target.running).toBe(false);
   });
+
+  it('should start timer', function () {
+    target.start();
+    expect(target.running).toBe(true);
+  });
+  
+  it('should reset timer', function() {
+    target.start();
+    target.reset();
+    expect(target.running).toBe(false);
+  });
+
+  it('should decrement timer', function() {
+    target.decrementSecond();
+    expect(target.timeLeftInMillis).toBe(29000)
+  });
+
+  it('should run clock until end', function() {
+    jasmine.Clock.useMock();
+    target.start();
+    jasmine.Clock.tick(30000);
+    expect(target.timeLeftInMillis).toBe(0);
+    expect(target.running).toBe(false);
+  });
+  
 });
