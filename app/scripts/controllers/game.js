ca75f4df323ca97ad98c3bbba7f1874a3f8c180f
Kalle Bertell <carl-petter.bertell@avaus.fi>
2013-11-01 20:23:04 +0200

    fleshed out game

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,11 +1,28 @@
 'use strict';
 
-angular.module('vagrantApp').controller('GameCtrl', function ($scope, state) {
+angular.module('vagrantApp').controller('GameCtrl', function ($scope, $location, $log, state) {
   $scope.state = state;
   $scope.message = 'Hello!';
 
-  $scope.nextRound = function() {
-    state.currentGameRound = state.currentGameRound + 1;
-    console.log(state.currentGameRound)
+  $scope.options = [
+    { label: 'Kalle Roxx', value: 'kalle'},
+    { label: 'Anssi Coxx', value: 'anssi'},
+    { label: 'Joku Muu', value: 'joku'}
+  ]
+
+  $scope.select = function(optionId) {
+    $log.info("Selected " + optionId);
+    nextRound();
   }
+
+  var nextRound = function() {
+    state.currentGameRound = state.currentGameRound + 1;
+
+    if (state.currentGameRound > state.gameRounds) {
+      $location.path("/score");
+    }
+
+    $log.info("Current game round: " + state.currentGameRound)
+  }
+
 });
