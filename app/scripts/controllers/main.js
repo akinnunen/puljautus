62ba116a2ef5d683137e57f42181b0bc5cf95aaf
Markus Sammallahti <markus.sammallahti@avaus.fi>
2013-11-01 20:23:15 +0200

    Game mode select functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,30 +3,33 @@
 angular.module('vagrantApp')
   .controller('MainCtrl', function ($scope, state, nameGenerator, $location) {
     $scope.gameModes = [
-    	{
-    		id: "names",
-    		name: "Names",
-    		enabled: true
-    	},
-    	{
-    		id: "projects",
-    		name: "Projects",
-    		enabled: false
-    	},
-    	{
-    		id: "competence",
-    		name: "Competence",
-    		enabled: false
-    	},
-    	{
-    		id: "other",
-    		name: "Something else",
-    		enabled: false
-    	}
+      {
+        id: "names",
+        name: "Names",
+        enabled: true
+      },
+      {
+        id: "projects",
+        name: "Projects",
+        enabled: false
+      },
+      {
+        id: "competence",
+        name: "Competence",
+        enabled: false
+      },
+      {
+        id: "other",
+        name: "Something else",
+        enabled: false
+      }
     ];
 
     $scope.play = function(mode) {
+      if (mode.enabled) {
+        state.mode = mode.id;
         $location.path('/game');
+      }
     }
 
     state.gameRounds = 4;
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,17 +6,41 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('vagrantApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    location,
+    state;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $location) {
     scope = $rootScope.$new();
+    location = $location;
+    location.url('/');
+    state = {};
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: scope,
+      state: state
     });
   }));
 
-  it('should attach a list of gameModes to the scope', function () {
+  it('should attach a list of game modes to the scope', function () {
     expect(scope.gameModes.length).toBe(4);
+  });
+
+  it('should set reset current game round', function() {
+    expect(state.currentGameRound).toBe(1);
+  });
+
+  it('should set game rounds', function() {
+    expect(state.gameRounds).toBe(4);
+  });
+
+  it('should change location to game when enabled mode is selected', function() {
+    scope.play({enabled: true});
+    expect(location.url()).toBe('/game');
+  });
+
+  it('should not change location to game when disabled mode is selected', function() {
+    scope.play({enabled: false});
+    expect(location.url()).toBe('/');
   });
 });
