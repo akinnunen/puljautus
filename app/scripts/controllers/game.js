e158a5c7cf71099a0cce1fbb49565b3b7dcb7d15
Juha Kuusela <juha.kuusela@avaus.fi>
2013-11-01 19:01:40 +0200

    Implement iterating through rounds in game screen

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -2,5 +2,10 @@
 
 angular.module('vagrantApp').controller('GameCtrl', function ($scope, state) {
   $scope.state = state;
-  $scope.message = 'Hello!'
+  $scope.message = 'Hello!';
+
+  $scope.nextRound = function() {
+    state.currentGameRound = state.currentGameRound + 1;
+    console.log(state.currentGameRound)
+  }
 });
