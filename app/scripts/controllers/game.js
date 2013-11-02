fde8229d278807861dd120e7a3613348088a3246
Anssi Kinnunen <anssi.kinnunen@gmail.com>
2013-11-02 12:15:45 +0200

    Display timer on game page and reset it for each round.

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,15 +1,24 @@
 'use strict';
 
-angular.module('vagrantApp').controller('GameCtrl', function ($scope, $location, $log, state, $filter, nameGenerator) {
+angular.module('vagrantApp').controller('GameCtrl', function ($scope, $location, $log, state, $filter, nameGenerator, roundTimer) {
+  
   if (state.mode === undefined) {
     $location.path('/');
+    return;
   }
 
   $scope.state = state;
-  $scope.message = 'Hello!';
+  $scope.timer = roundTimer;
 
+  // Fetch current game mode fronm the JSON
   var data = $filter('getByMode')(state.gameModes, state.mode).options;
 
+   // Since $scope.$watch 'timer' does not work
+  setInterval(function() {
+    $scope.$apply();
+  }, 1000); 
+
+  // Called when player selects an option
   $scope.select = function(index) {
 
     $log.info("Selected " + index);
@@ -22,12 +31,17 @@ angular.module('vagrantApp').controller('GameCtrl', function ($scope, $location,
     }
 
     setTimeout(function() {
-      nextRound();
-      $scope.$apply();
+      $scope.$apply(function() {
+        nextRound();
+      });
     }, 1500);
   }
 
+  // Called when game starts or player has answered
   var nextRound = function() {
+
+    $scope.timer.reset();
+    $scope.timer.start();
 
     state.currentGameRound = state.currentGameRound + 1;
 
